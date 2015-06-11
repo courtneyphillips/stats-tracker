3f53b3278306633ab7732c42f3ee94911d3d4611
Courtney Phillips & Peter <student@epicodus.com>
2015-06-11 15:39:50 -0700

    courtney fixed.. peter still really sucks

--- models/player.js
+++ models/player.js
@@ -1,7 +1,7 @@
 StatsKeeper.Player = DS.Model.extend({
     playerName: DS.attr(),
     team: DS.belongsTo("team", {async: true}),
-    stats: DS.hasMany('stat', {async: true}),
+    stat: DS.hasMany('stat', {async: true}),
     shootingPercentage: function() {
 
 
@@ -20,6 +20,8 @@ StatsKeeper.Player = DS.Model.extend({
 
       }
 
+    } else{
+      console.log("you suck");
     }
 
     }.property("stats")
